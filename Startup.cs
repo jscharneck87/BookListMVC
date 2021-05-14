1ff7cd2dc2ed87430c7bccf039174ca3b70c1a92
jscharneck87 <jose.scharneck@outlook.com>
2021-05-14 10:20:04 +0200

    AddRazorPages

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace BookListMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
